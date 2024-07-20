eb7ea955c3472d80693ba70f3784285633f759de
Tornike Japaridze <88436296+japhridze@users.noreply.github.com>
2024-07-20 17:39:57 +0400

    Add files via upload

---
+++ MeetAStranger.java
@@ -0,0 +1,13 @@
+package com.epam.rd.autotasks.meetastranger;
+
+import java.util.Scanner;
+
+public class MeetAStranger {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name);
+           }
+
+}
